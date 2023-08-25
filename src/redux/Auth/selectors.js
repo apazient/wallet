3e754c8d3eb1e694becde90747228b5cc5d40855
apazient <nataliia.amanzhayeva@web.de>
2023-08-25 06:42:13 +0200

    create store, authSlice

---
+++ src/redux/Auth/selectors.js
@@ -0,0 +1,4 @@
+//getUser from state
+export const getUser = state => {
+  return state.user.user;
+};
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,2 +1,11 @@
-import { styled } from 'styled-components';
-export const GlobalStyles = styled.input``;
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyles = createGlobalStyle`
+
+body{
+    height: 100vh;
+   
+    background: radial-gradient(220deg, #6D54EB 3.47%, #652392 90.06%);
+
+    
+}
+`;
