4747232b59bb1b8e624bddfb9653154bfa04c2d1
zhurakovska <n.sinawka@gmail.com>
2023-08-25 12:15:02 +0300

    styles for navigation
    1) поки немає routes  то замість NavLink поставила тимчасово тег a, щоб не вибивало помилку
    2) замість іконок заглушка
    3) не знайшла ще шрифти , потрібно буде додати

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import {
+  NavIconStyled,
+  NavLinkStyled,
+  NavLinkWrapperStyled,
+  NavWrapperStyled,
+} from './Navigation.styled';
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <NavWrapperStyled>
+      <NavLinkWrapperStyled>
+        <NavIconStyled></NavIconStyled>
+        <NavLinkStyled href="/">Home</NavLinkStyled>
+      </NavLinkWrapperStyled>
+
+      <NavLinkWrapperStyled>
+        <NavIconStyled></NavIconStyled>
+        <NavLinkStyled href="/">Statistics</NavLinkStyled>
+      </NavLinkWrapperStyled>
+    </NavWrapperStyled>
+  );
 };
 
 export default Navigation;
