99c3dea71c32d904c1b1e6c9c209ea400ac2f9a8
Roman Serdiuk <roman.srdk21@gmail.com>
2023-08-26 10:55:21 +0300

    RegistrationForm
    no styles

--- src/components/RegisrationForm/RegisrationForm.jsx
+++ src/components/RegisrationForm/RegisrationForm.jsx
@@ -2,37 +2,11 @@ import React from 'react';
 import { SpriteSVG } from 'pictures/SpriteSVG';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/Auth/operations';
 
 const RegisrationForm = () => {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [pwd, setPwd] = useState('');
-  const [firstName, setFirstName] = useState('');
-
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    switch (name) {
-      case 'email':
-        setEmail(value);
-        break;
-      case 'password':
-        setPassword(value);
-        break;
-      case 'pwd':
-        setPwd(value);
-        break;
-      case 'firstName':
-        setFirstName(value);
-        break;
-      default:
-        console.log('Value not found');
-        return;
-    }
-  };
+  // const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -49,89 +23,88 @@ const RegisrationForm = () => {
 
   return (
     <FormContainer>
-      <StyledDivItems>
-        <SpriteSVG name={'logo'} />
-        <StyledH2>Money Guard</StyledH2>
-      </StyledDivItems>
-      <Formik
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-        validationSchema={Yup.object({
-          email: Yup.string()
-            .email('Invalid email address')
-            .required('Required'),
-          password: Yup.string()
-            .min(6)
-            .max(12, 'Must be 12 characters or less')
-            .required('Required'),
-          pwd: Yup.string()
-            .min(6)
-            .max(12, 'Must be 12 characters or less')
-            .required('Required'),
-          firstName: Yup.string()
-            .min(2)
-            .max(15, 'Must be 15 characters or less')
-            .required('Required'),
-        })}
-      >
-        <Form onSubmit={handleSubmit}>
-          <label>
-            <SpriteSVG name={'email'} />
-            <Field
-              onChange={handleChange}
-              type="email"
-              name="email"
-              placeholder="E-mail"
-              value={email}
-              autofocus
-            />
-            <ErrorMessage name="email" />
-          </label>
+      <StyledDivContainer>
+        <StyledDivItems>
+          <SpriteSVG name={'logo'} />
+          <StyledH2>Money Guard</StyledH2>
+        </StyledDivItems>
+        <Formik
+          initialValues={{ email: '', password: '', pwd: '', firstName: '' }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+          validationSchema={Yup.object({
+            email: Yup.string()
+              .email('Invalid email address')
+              .required('Required'),
+            password: Yup.string()
+              .min(6, 'Password is too short')
+              .max(12, 'Must be 12 characters or less')
+              .required('Required'),
+            pwd: Yup.string()
+              .min(6, 'Password is too short')
+              .max(12, 'Must be 12 characters or less')
+              .required('Required'),
+            firstName: Yup.string()
+              .min(2)
+              .max(15, 'Must be 15 characters or less')
+              .required('Required'),
+          })}
+        >
+          <Form onSubmit={handleSubmit}>
+            <label>
+              <SpriteSVG name={'email'} />
+              <Field
+                onChange={handleChange}
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                autofocus
+              />
+              <ErrorMessage name="email" />
+            </label>
 
-          <label>
-            <SpriteSVG name={'password'} />
-            <Field
-              onChange={handleChange}
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-            />
-            <ErrorMessage name="password" />
-          </label>
+            <label>
+              <SpriteSVG name={'password'} />
+              <Field
+                onChange={handleChange}
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <ErrorMessage name="password" />
+            </label>
 
-          <label>
-            <SpriteSVG name={'password'} />
-            <Field
-              onChange={handleChange}
-              type="password"
-              name="pwd"
-              placeholder="Confirm password"
-              value={pwd}
-            />
-            <ErrorMessage name="pwd" />
-          </label>
+            <label>
+              <SpriteSVG name={'password'} />
+              <Field
+                onChange={handleChange}
+                type="password"
+                name="pwd"
+                placeholder="Confirm password"
+              />
+              <ErrorMessage name="pwd" />
+            </label>
 
-          <label>
-            <SpriteSVG name={'name'} />
-            <Field
-              onChange={handleChange}
-              type="text"
-              name="firstName"
-              placeholder="First name"
-              value={firstName}
-            />
-            <ErrorMessage name="firstName" />
-          </label>
+            <label>
+              <SpriteSVG name={'name'} />
+              <Field
+                onChange={handleChange}
+                type="text"
+                name="firstName"
+                placeholder="First name"
+              />
+              <ErrorMessage name="firstName" />
+            </label>
 
-          <button type="submit">Register</button>
-          <button type="submit">Log in</button>
-        </Form>
-      </Formik>
+            <button type="submit">Register</button>
+            <button type="submit">Log in</button>
+          </Form>
+        </Formik>
+      </StyledDivContainer>
     </FormContainer>
   );
 };
