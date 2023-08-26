ec08205e212932ea97b9007bece28a2abe144240
Taras Kovalyk <tarik2454@gmail.com>
2023-08-26 19:00:49 +0300

    Fix

--- src/redux/Auth/operations.js
+++ src/redux/Auth/operations.js
@@ -5,21 +5,16 @@ export const API = axios.create({
   baseURL: 'https://wallet.goit.ua',
 });
 
-// const setToken = token => {
-//   API.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
-API.defaults.headers.common.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiJmMWYxMzBkNC1mOTZkLTRlMjItODYxMi05M2E4ZTk4MDRlOWYiLCJpYXQiOjE2OTI5NTc4MzIsImV4cCI6MTAwMDAwMDE2OTI5NTc4MzJ9.TRIOKOxy8JoZt1_EI9b2CkkaPjCdzK5u0Ty9gT6ol00`;
-
-// const clearToken = () => {
-//   API.defaults.headers.common.Authorization = ``;
-// };
+const setToken = token => {
+  API.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 export const register = createAsyncThunk(
   'auth/reg',
   async (credentials, thunkAPI) => {
     try {
       const res = await API.post('/api/auth/sign-up', credentials);
-      // setToken(res.data.token);
+      setToken(res.data.token);
       console.log(res.data);
       return res.data;
     } catch (error) {
@@ -33,7 +28,7 @@ export const loginThunk = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const res = await API.post('/api/auth/sign-in', credentials);
-      // setToken(res.data.token);
+      setToken(res.data.token);
       console.log(res.data);
       return res.data;
     } catch (error) {
