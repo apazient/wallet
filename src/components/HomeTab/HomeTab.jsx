3bd3c588807a3a5df3024e7ef1514b5486cdc095
apazient <nataliia.amanzhayeva@web.de>
2023-08-26 13:18:41 +0200

    add TAbleList to home

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,6 +1,7 @@
 import ButtonAddTransactions from 'components/ButtonAddTransactions/ButtonAddTransactions';
 import Modal from 'components/Modal/Modal';
 import ModalAddTransaction from 'components/ModalAddTransaction/ModalAddTransaction';
+import TransactionsList from 'components/TransactionsList/TransactionsList';
 import React from 'react';
 
 import { useSelector } from 'react-redux';
@@ -12,6 +13,7 @@ const HomeTab = () => {
   return (
     <div>
       <h1>HomeTab</h1>
+      <TransactionsList />
       <ButtonAddTransactions />
       {isAddTrans && (
         <Modal>
