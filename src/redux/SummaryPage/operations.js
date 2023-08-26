e7704a890b0f4b14ae20e8c752b232cb321f2b42
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-08-26 23:42:17 +0300

    SummaryPage+++

--- src/redux/SummaryPage/operations.js
+++ src/redux/SummaryPage/operations.js
@@ -5,11 +5,9 @@ export const feachTransactionSummary = createAsyncThunk(
   'summary/feachTransactionSummary',
   async (credentials, thunkAPI) => {
     try {
-      console.log(credentials);
       const res = await API.get('/api/transactions-summary', {
         params: credentials,
       });
-      console.log(res.data);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
