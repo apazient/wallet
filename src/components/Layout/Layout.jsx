4818163c31af278047ad7c9f4d7acd3a4d043712
Taras Kovalyk <tarik2454@gmail.com>
2023-08-27 19:01:07 +0300

    fix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,8 @@
-import Balance from 'components/Balance/Balance';
 import Header from 'components/Header/Header';
 import Navigation from 'components/Navigation/Navigation';
 import DashboardPage from 'pages/DashboardPage/DashboardPage';
 import React from 'react';
 import { Outlet } from 'react-router';
-import { Container } from 'styles/GlobalStyle';
-import { useMediaQuery } from 'react-responsive';
 
 const Layout = () => {
   return (
@@ -17,44 +14,5 @@ const Layout = () => {
     </>
   );
 };
-
-// const Layout = () => {
-//   return (
-//     <>
-//       <Header />
-//       {isTablet && (
-//         <Container>
-//           <Navigation />
-//           <Balance />
-//         </Container>
-//       )}
-//       <DashboardPage />
-//       <Outlet />
-//     </>
-//   );
-// };
-
-// {
-//   isTablet && (
-//     <Container>
-//       {' '}
-//       тут display-flex
-//       <NavWithBalanceContainer>
-//         <Navigation />
-//         <Balance />
-//       </NavWithBalanceContainer>
-//       <Currency />
-//     </Container>
-//   );
-// }
-// {
-//   isDesktop && (
-//     <Container>
-//       <Navigation />
-//       <Balance />
-//       <Currency />
-//     </Container>
-//   );
-// }
 
 export default Layout;
