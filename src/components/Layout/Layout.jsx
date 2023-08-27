6a66dcae01e6a133935a5bfa2955968a23051ac9
Taras Kovalyk <tarik2454@gmail.com>
2023-08-27 18:14:00 +0300

    Comit

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,11 @@
+import Balance from 'components/Balance/Balance';
 import Header from 'components/Header/Header';
 import Navigation from 'components/Navigation/Navigation';
 import DashboardPage from 'pages/DashboardPage/DashboardPage';
 import React from 'react';
 import { Outlet } from 'react-router';
+import { Container } from 'styles/GlobalStyle';
+import { useMediaQuery } from 'react-responsive';
 
 const Layout = () => {
   return (
@@ -14,5 +17,44 @@ const Layout = () => {
     </>
   );
 };
+
+// const Layout = () => {
+//   return (
+//     <>
+//       <Header />
+//       {isTablet && (
+//         <Container>
+//           <Navigation />
+//           <Balance />
+//         </Container>
+//       )}
+//       <DashboardPage />
+//       <Outlet />
+//     </>
+//   );
+// };
+
+// {
+//   isTablet && (
+//     <Container>
+//       {' '}
+//       тут display-flex
+//       <NavWithBalanceContainer>
+//         <Navigation />
+//         <Balance />
+//       </NavWithBalanceContainer>
+//       <Currency />
+//     </Container>
+//   );
+// }
+// {
+//   isDesktop && (
+//     <Container>
+//       <Navigation />
+//       <Balance />
+//       <Currency />
+//     </Container>
+//   );
+// }
 
 export default Layout;
