88505b1cadb3b43b8bc2fb23a73f2974a64fbd15
apazient <nataliia.amanzhayeva@web.de>
2023-08-28 12:24:06 +0200

    update login logik

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,27 @@ import { PublicRoute } from 'HOC/PublicRoute/PublicRoute';
 import LoginPage from 'pages/LoginPage/LoginPage';
 import NotFound from 'pages/NotFound/NotFound';
 import { PrivateRoute } from 'HOC/PrivateRoute/PrivateRoute';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { currentUser } from 'redux/Auth/operations';
 import { Currency } from './Currency/Currency';
+import { getIsAuth } from 'redux/Auth/selectors';
+import { Navigate } from 'react-router';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector(getIsAuth);
   useEffect(() => {
     dispatch(currentUser());
   }, [dispatch]);
+  console.log(isLoggedIn);
+
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route
+        path="/"
+        element={isLoggedIn ? <Layout /> : <Navigate to="/login" />}
+      >
         <Route
           index
           element={
@@ -36,33 +44,32 @@ export const App = () => {
             </PrivateRoute>
           }
         />
-        <Route 
-        path="currency"
-        element={
-          <PrivateRoute>
-            <Currency/>
-          </PrivateRoute>
-        }
-        />
         <Route
-          path="register"
+          path="currency"
           element={
-            <PublicRoute>
-              <RegistrationPage />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="login"
-          element={
-            <PublicRoute>
-              <LoginPage />
-            </PublicRoute>
+            <PrivateRoute>
+              <Currency />
+            </PrivateRoute>
           }
         />
       </Route>
+      <Route
+        path="register"
+        element={
+          <PublicRoute>
+            <RegistrationPage />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="login"
+        element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
-
 };
