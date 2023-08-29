7655aa2eb6340f1441eb541a7c66c4883b039d51
apazient <nataliia.amanzhayeva@web.de>
2023-08-29 07:37:06 +0200

    update overlay Register

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import {
-  OverlayGradient11,
-  OverlayGradient12,
-  OverlayGradient13,
-  OverlayGradient14,
-  OverlayGradient15,
-  OverlayGradient16,
-  RegistrOverlay,
-} from './RegistrationPage.styled';
+import { RegistrOverlay } from './RegistrationPage.styled';
 import RegistrationForm from '../../components/RegisrationForm/RegisrationForm';
+import { useMediaQuery } from 'react-responsive';
+import { ISDESKTOP, ISMOBILE, ISTABLET } from 'styles/const ';
+import {
+  Overlay14,
+  Overlay16,
+  Overlay17,
+  Overlay18,
+  Overlay19,
+  Overlay20,
+} from 'components/Main/Main.styled';
 
 const RegistrationPage = () => {
+  const isMobile = useMediaQuery(ISMOBILE);
+  const isTablet = useMediaQuery(ISTABLET);
+  const isDesctop = useMediaQuery(ISDESKTOP);
   return (
-    <RegistrOverlay>
-      <RegistrationForm />
-      <OverlayGradient11></OverlayGradient11>
-      <OverlayGradient12></OverlayGradient12>
-      <OverlayGradient13></OverlayGradient13>
-      <OverlayGradient14></OverlayGradient14>
-      <OverlayGradient15></OverlayGradient15>
-      <OverlayGradient16></OverlayGradient16>
-    </RegistrOverlay>
+    <>
+      {isMobile && (
+        <>
+          <RegistrationForm />
+          <Overlay16 />
+          <Overlay18 />
+          <Overlay14 />
+          <Overlay19 />
+          <Overlay17 />
+          <Overlay20 />
+        </>
+      )}
+
+      {(isTablet || isDesctop) && (
+        <RegistrOverlay>
+          <RegistrationForm />
+          <Overlay16 />
+          <Overlay18 />
+          <Overlay14 />
+          <Overlay19 />
+          <Overlay17 />
+          <Overlay20 />
+        </RegistrOverlay>
+      )}
+    </>
   );
 };
 
