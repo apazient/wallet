673da325af20e1063e38ad2380e39bdd27f6339a
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-08-29 17:59:56 +0300

    comit
    +++

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -7,12 +7,17 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { isAddTransaction } from 'redux/Global/selectors';
 import { Warapper } from './HomeTab.styled';
+import { useMediaQuery } from 'react-responsive';
+import { IS_MOBILE } from 'styles/const ';
+import Balance from 'components/Balance/Balance';
 
 const HomeTab = () => {
   const isAddTrans = useSelector(isAddTransaction);
+  const isMobile = useMediaQuery(IS_MOBILE);
 
   return (
     <Warapper>
+      {isMobile && <Balance />}
       <TransactionsList />
       <ButtonAddTransactions />
       {isAddTrans && (
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -4,20 +4,13 @@ import { DashboardContainerStyled } from './DashboardPage.styled';
 import { Currency } from 'components/Currency/Currency';
 import Balance from 'components/Balance/Balance';
 import { useMediaQuery } from 'react-responsive';
-import { IS_DESKTOP, IS_MOBILE, IS_TABLET } from 'styles/const ';
+import { IS_DESKTOP, IS_TABLET } from 'styles/const ';
 
 export const DashboardPage = () => {
-  const isMobile = useMediaQuery(IS_MOBILE);
   const isTablet = useMediaQuery(IS_TABLET);
   const isDesktop = useMediaQuery(IS_DESKTOP);
   return (
     <>
-      {isMobile && (
-        <DashboardContainerStyled>
-          <Balance />
-        </DashboardContainerStyled>
-      )}
-
       {(isTablet || isDesktop) && (
         <DashboardContainerStyled>
           <Balance />
